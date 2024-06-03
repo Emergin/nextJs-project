e62e0942fa3390841de2ddf86107135472e42ead
Emergin <ngangaephantus112@gmail.com>
2024-06-03 16:36:35 +0530

    next js project

--- src/app/(dashboard)/_components/sidebar-routes.tsx
+++ src/app/(dashboard)/_components/sidebar-routes.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { SidebarItem } from "./sidebar-item";
-import { Compass, Layout } from "lucide-react";
+import { BarChart, List, Compass, Layout } from "lucide-react";
+import { usePathname } from "next/navigation";
 
 const guestRoutes = [
     {
@@ -13,9 +14,26 @@ const guestRoutes = [
         label: "Browse",
         href: "/search",
     }
+];
+
+const teacherRoutes = [
+    {
+        icon: List,
+        label: "Courses",
+        href: "/teacher/courses",
+    },
+    {
+        icon: BarChart,
+        label: "Analytics",
+        href: "/teacher/analytics",
+    }
 ]
+
 export const SidebarRoutes = () => {
-const routes = guestRoutes;
+const pathname = usePathname()
+const isTeacherPage = pathname?.includes("/teacher")
+
+const routes = isTeacherPage ? teacherRoutes : guestRoutes;
 
     return (
         <div className="flex flex-col w-full">
